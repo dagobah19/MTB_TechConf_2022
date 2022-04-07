3d5d2801bcb4ca0b6a5f75b607ddef9b80b3066a
dagobah19 <dorlowski@gmail.com>
2022-04-07 15:08:37 -0400

    basic websocket implementation

--- server/index.js
+++ server/index.js
@@ -1,24 +1,114 @@
+/*
+* First we define variables and require for the express api server to work
+*/
+
 const url = require('url');
 var express = require('express');
 var cors = require('cors')
 var app = express();
 const bodyParser = require('body-parser');
+
+//allow CORS for all requests
+app.use(cors()) 
+//use json as our body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*
+* Next we define our database variables, Cassandra
+*/
+
 const { Client } = require('cassandra-driver');
 var cassandra = require('cassandra-driver'); 
 var auth = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra');
 var contactPoints = ['localhost'];
-const port=3001
 
-//allow CORS for all requests
-app.use(cors()) 
+/*
+* Miscellanous imports we need
+*/
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var moment = require('moment');
+const { timeStamp } = require('console');
+
+/*
+* Define the ports we will listen on
+*/
+
+const serverPort=3005
+const socketPort=3006
+
+/*
+* START connections & servers
+*/
+
+//Cassandra
 
 var client = new cassandra.Client({contactPoints: contactPoints, 
     localDataCenter: 'datacenter1', 
     authProvider: auth, keyspace:'sensordata'
 });
+
+//Websocket
+
+const webSocketServer = require('websocket').server;
+const http = require('http');
+const server = http.createServer();
+server.listen(socketPort, function(){
+    console.log("listening to socket server on %d",socketPort)
+});
+
+const wsServer = new webSocketServer({
+    httpServer: server
+});
+
+//http express server
+app.listen(serverPort, function () {
+    console.log('Server app listening on port %d',serverPort);
+});
+  
+/*
+* WEBSOCKET listeners
+*/
+
+//this object will hold our clients connection information
+const clients = {};
+
+//generate a unique client id for incoming requests / connections
+const getUniqueID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + s4();
+};
+
+//send data to all connected clients
+const sendMessage = (message) => {
+    Object.keys(clients).map((client) => {
+      clients[client].send(message);
+    });
+}
+
+wsServer.on('request', function(request) {
+    var userID = getUniqueID();
+    console.log((new Date()) + ' Received new connection from origin ' + request.origin + '.');
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+
+    connection.on('message', function(message) {
+        //sendMessage(JSON.stringify({"message":message}))
+        console.log(message);
+        sendMessage(message)
+    });
+    // user disconnected
+    connection.on('close', function(connection) {
+        console.log((new Date()) + userID + " disconnected.");
+        delete clients[userID];
+        sendMessage(userID + " disconnected.");
+    });
+});
+
+/*
+* HTTP LISTENERS
+*/
 
 /*  
 *   Define an action and return for the root
@@ -27,6 +117,8 @@ var client = new cassandra.Client({contactPoints: contactPoints,
 */
 app.get('/', function (req, res) {
     res.send('Not implemented');
+    //res.sendStatus(200)
+
 });
 
 /*  
@@ -53,7 +145,10 @@ app.post('/sensordata/:sensor', function(req,res){
         }
     })
     if (timestamp){
+        console.log(timestamp)
         //var datetime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:MM:ss");
+        var momentDate = moment(timestamp).format("YYYY-MM-DD HH:MM:ss.SSSSSS");
+        var query = "INSERT INTO unstructured_sensordata (sensor_name,entry_date,data) VALUES ('"+sensorName+"','"+momentDate+"',"+data+")"
     } else {
         var query = "INSERT INTO unstructured_sensordata (sensor_name,entry_date,data) VALUES ('"+sensorName+"',toTimeStamp(now()),"+data+")"
     }
@@ -101,6 +196,7 @@ app.get('/sensordata/:sensor', function(req,res) {
 
 });
 
+
 /*  
 *   Define a 404 error message & route to catch the rest of possible queries
 *   we do this so express will not "hang" when it comes across an unknown route
@@ -110,7 +206,3 @@ app.use(function(req, res, next) {
     res.status(404).send("Not Found");
 });
 
-// start server on port
-app.listen(port, function () {
-    console.log('Server app listening on port ' + port);
-});
